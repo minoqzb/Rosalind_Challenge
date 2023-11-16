60ef3df40749fb9596c93897a8c6e2c31d94e98e
minoqzb <mmin926@gmail.com>
2023-11-16 23:04:26 +0100

    solved

---
+++ FIB_Rabbits_and_Recurrence_Relations/Rabbits_and_Recurrence_Relations.py
@@ -0,0 +1,16 @@
+def getNumberOfRabbitsRec(month, offspring):
+    if month == 1 or month == 2:
+        return 1
+    else:
+        return getNumberOfRabbitsRec(month - 1, offspring) + getNumberOfRabbitsRec(month - 2, offspring) * offspring
+
+f = open("rosalind_fib.txt", "r")
+int_arr = f.read().split(" ")
+f.close()
+
+n = int(int_arr[0])
+k = int(int_arr[1])
+
+f = open("rosalind_fib_output.txt", "w")
+f.write(str(getNumberOfRabbitsRec(n, k)))
+f.close()
